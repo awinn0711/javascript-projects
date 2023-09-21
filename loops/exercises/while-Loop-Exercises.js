4e024f1706585f92b311b2c6b5250ba7a17ad8f3
Austin Winn <awinn0711@gmail.com>
2023-09-21 18:46:40 -0500

    added validation to prevent non numbers from being entered by user

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -11,7 +11,7 @@ let altitude = 0
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but
  less than 30000. */
  startingFuel = Number(input.question("What is the starting fuel level?"));
-while (startingFuel<5000 || startingFuel>30000) {
+while (startingFuel<5000 || startingFuel>30000 || isNaN(startingFuel)) {
   console.log("Please enter a number between 5,000 and 30,000.")
   startingFuel = Number(input.question("What is the starting fuel level?"));
 }
@@ -19,7 +19,7 @@ while (startingFuel<5000 || startingFuel>30000) {
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
 astronautCount = Number(input.question("How many astronauts are on board?(max 7)"));
-while (astronautCount<0 || astronautCount>7) {
+while (astronautCount<0 || astronautCount>7 || isNaN(astronautCount)) {
   console.log("The maximum number of astronauts that can board this shuttle is seven.");
   astronautCount = Number(input.question("How many astronauts are on board?(max 7)"));
 }
@@ -27,7 +27,7 @@ while (astronautCount<0 || astronautCount>7) {
   /*c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel 
 level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers. */
 
-while (startingFuel>0) {
+while (startingFuel> astronautCount * 100) {
   altitude += 50;
   startingFuel -= 100 * astronautCount;
 }
