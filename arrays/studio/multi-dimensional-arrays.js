12e725deb0d916b681a4a7e837e43674e49c0807
Austin Winn <awinn0711@gmail.com>
2023-09-24 17:02:59 -0500

    trying to update readline-sync

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,4 +1,4 @@
-const input = require('readline.sync');
+const input = require('readline-sync');
 
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
@@ -6,23 +6,28 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-let cabinet1 = [food.split(",")];
-let cabinet2 = [equipment.split(",")];
-let cabinet3 = [pets.split(",")];
-let cabinet4 = [sleepAids.split(",")];
+let cabinet0 = [food.split(",")];
+let cabinet1 = [equipment.split(",")];
+let cabinet2 = [pets.split(",")];
+let cabinet3 = [sleepAids.split(",")];
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-let cargoHold = [cabinet1, cabinet2, cabinet3, cabinet4];
+let cargoHold = [cabinet0, cabinet1, cabinet2, cabinet3];
 console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-let selectedCabinet = number(input.question("The cargo hold contains four cabinets. Please select one to view its contents."));
+let selectedCabinet = Number(input.question("The cargo hold contains four cabinets. Please select a cabinet number 0 to 3 to search its contents."));
+let selectedItem = input.question("What item are you searching for?").toLowerCase();
+
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if (selectedCabinet < 1 || selectedCabinet > 4) {
-    console.log("Error. Please select a cabinet number 1 through 4.");
+if (selectedCabinet < 0 || selectedCabinet > 3) {
+    console.log("Error. Please select a cabinet number 0 to 3.");
+} else if (cargoHold[selectedCabinet].includes(selectedItem)) {
+    console.log(`Cabinet ${selectedCabinet} contains ${selectedItem}.`)
 } else {
-    console.log(`${selectedCabinet} contains ${cargoHold[selectedCabinet - 1]}.`)
+    console.log(`Cabinet ${selectedCabinet} does NOT contain ${selectedItem}`);
 }
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
