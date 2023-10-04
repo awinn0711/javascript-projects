7dde72ef0e3acd11f909132eb497d20cdac8031b
Austin Winn <awinn0711@gmail.com>
2023-10-03 23:27:14 -0500

    completed modules exercises

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,10 @@
 function randomFromArray(arr){
-  //Your code here to select a random element from the array passed to the function.
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
 
+let animalArray = ["dog", "cat", "owl", "rabbit"];
+//.log(randomFromArray(animalArray));
+
 //TODO: Export the randomFromArray function.
+module.exports = randomFromArray;
