9008ad4aaec5848e6c780e97869f80f1bde07538
Austin Winn <awinn0711@gmail.com>
2023-10-28 15:03:36 -0500

    completed dom exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", event => {
+        missionAbort.style.backgroundColor = "red"; 
+    });
+
+    missionAbort.addEventListener("mouseout", event => {
+        missionAbort.style.backgroundColor = "";
+    });
     
-}
+    missionAbort.addEventListener("click", event => {
+        if (window.confirm("Are you sure you want to abort the mission?")) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        };
+    });
+  
+};
 
 window.addEventListener("load", init);
